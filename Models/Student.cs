fae87f156c88dd986dd439309fda2ae15a743424
yaple3 <yaple3@JB204-L26-W-D>
2024-04-27 00:49:13 -0400

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; } = string.Empty;
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+       // public string Secret { get; set; } 
+
+        public ICollection<Enrollment> Enrollments { get; set; } = default!;
+    }
+}
